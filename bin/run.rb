3185abe09aef14907cb3cbba48285a0f585032a2
Qing <chingwang.qw@gmail.com>
2019-11-12 11:03:46 +0000

    add testing line into run.rb and delete lib directory

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,24 @@
 require_relative '../config/environment'
-
+require "pry"
 cli = CLI.new
 cli.greet
+
+microsoft = Stock.find_or_create_by(symbol: "MSFT")
+microsoft.save
+amazon = Stock.find_or_create_by(symbol: "AMZN")
+amazon.save
+google = Stock.find_or_create_by(symbol: "GOOG")
+google.save
+
+portfolio = Portfolio.new
+portfolio.save
+
+portfolio.positions.create(stock_id: microsoft.id)
+
+binding.pry
+foo = "bar"
+
+puts portfolio.total_value
+
 
 puts "HELLO WORLD"
