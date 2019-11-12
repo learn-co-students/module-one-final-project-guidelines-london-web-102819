e6e4608d1b227e462422050d5d035edf4f8675ab
Qing <chingwang.qw@gmail.com>
2019-11-12 22:54:40 +0000

    seeding position and portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@
 
 # Test stocks
 
-Stock.find_or_create_by(company_name: "Apple", symbol: "AAPL")
-Stock.find_or_create_by(company_name: "Microsoft", symbol: "MSFT")
-Stock.find_or_create_by(company_name: "Google", symbol: "GOOG")
-Stock.find_or_create_by(company_name: "Amazon", symbol: "AMZN")
-Stock.find_or_create_by(company_name: "Tesla", symbol: "TSLA")
+apple = Stock.find_or_create_by(company_name: "Apple", symbol: "AAPL")
+microsoft = Stock.find_or_create_by(company_name: "Microsoft", symbol: "MSFT")
+google = Stock.find_or_create_by(company_name: "Google", symbol: "GOOG")
+amazon = Stock.find_or_create_by(company_name: "Amazon", symbol: "AMZN")
+tesla = Stock.find_or_create_by(company_name: "Tesla", symbol: "TSLA")
+
 
 # Test user
 
@@ -24,6 +25,37 @@ user2 = User.find_or_create_by(
   password: "12345678"
 )
 
+user3 = User.find_or_create_by(
+  first_name: "Andy",
+  last_name: "Lewell",
+  email: "andy@gmail.com",
+  password: "topsecrect"
+)
 
+# Test portfolio
+
+portfolio1 = Portfolio.find_or_create_by( user_id: user1.id)
+portfolio2 = Portfolio.find_or_create_by( user_id: user2.id)
+portfolio3 = Portfolio.find_or_create_by( user_id: user3.id)
+
+
+# Test position
+
+position1 = Position.find_or_create_by(
+  portfolio_id: portfolio1,
+  stock_id: apple,
+  quantity: 80
+)
+
+position2 = Position.find_or_create_by(
+  portfolio_id: portfolio2,
+  stock_id: microsoft,
+  quantity: 15
+)
+position3 = Position.find_or_create_by(
+  portfolio_id: portfolio3,
+  stock_id: google,
+  quantity: 223
+)
 
 p 'seeds done'
